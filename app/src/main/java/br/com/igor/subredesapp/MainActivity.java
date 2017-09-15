b79735ad4c925c886c1a85ae801eae84425dec1e
Igor <Igorrafael20102010@gmail.com>
2017-09-15 18:26:32 -0300

    no message

--- app/src/main/java/br/com/igor/subredesapp/MainActivity.java
+++ app/src/main/java/br/com/igor/subredesapp/MainActivity.java
@@ -13,8 +13,10 @@ import br.com.igor.subredesapp.model.Subrede;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String IP;
+    private String ip;
     private String mascara;
+    private Integer numeroIP;
+    private Integer numeroMascara;
     private Subrede subrede;
     private ListView listView;
     ListAdapter customAdapter;
@@ -27,17 +29,96 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listViewTables);
 
         // get data from the table by the ListAdapter
-        customAdapter = new ListAdapter(this, R.layout.listview_table, );
+//        customAdapter = new ListAdapter(this, R.layout.listview_table, );
 
         listView.setAdapter(customAdapter);
 
     }
 
+
+//    https://www.codeproject.com/Tips/850531/IP-Subnet-Calculator-Lib
+
     public void calcular(View view) {
 
         EditText editText1 = (EditText) findViewById(R.id.editTextIP);
-        IP = editText1.getText().toString();
+        ip = editText1.getText().toString();
         EditText editText2 = (EditText) findViewById(R.id.editTextMascara);
         mascara = editText2.getText().toString();
+
+        // IP
+        String[] st = ip.split(".");
+
+        if (st.length != 4)
+            throw new NumberFormatException(R.string.ipInvalido + ip);
+
+        Integer i = 24;
+        numeroIP = 0;
+
+        for (int n = 0; n < st.length; n++) {
+
+            int value = Integer.parseInt(st[n]);
+
+            if (value != (value & 0xff)) {
+
+                throw new NumberFormatException(R.string.ipInvalido + ip);
+            }
+
+            numeroIP += value << i;
+            i -= 8;
+        }
+
+        // Mascara
+
+                /* Netmask */
+        st = mascara.split(".");
+
+        if (st.length != 4)
+            throw new NumberFormatException(R.string.mascaraInvalida
+
+                    + mascara);
+
+        i = 24;
+        numeroMascara = 0;
+
+        if (Integer.parseInt(st[0]) < 255) {
+
+            throw new NumberFormatException(
+                    R.string.fistByteMascara + "");
+        }
+        for (int n = 0; n < st.length; n++) {
+
+            int value = Integer.parseInt(st[n]);
+
+            if (value != (value & 0xff)) {
+
+                throw new NumberFormatException(R.string.mascaraInvalida + mascara);
+            }
+
+            numeroMascara += value << i;
+            i -= 8;
+
+        }
+/*
+* see if there are zeroes inside netmask, like: 1111111101111 This is
+* illegal, throw exception if encountered. Netmask should always have
+* only ones, then only zeroes, like: 11111111110000
+*/
+        boolean encounteredOne = false;
+        int ourMaskBitPattern = 1;
+
+        for (i = 0; i < 32; i++) {
+
+            if ((numeroMascara & ourMaskBitPattern) != 0) {
+
+                encounteredOne = true; // the bit is 1
+            } else { // the bit is 0
+                if (encounteredOne == true)
+
+                    throw new NumberFormatException(R.string.mascaraInvalida +
+                            mascara + " (bit " + (i + 1) + ")");
+            }
+
+            ourMaskBitPattern = ourMaskBitPattern << 1;
+        }
     }
 }
